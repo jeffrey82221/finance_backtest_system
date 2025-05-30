6b820c92328b2ddb0558aba11c637d5cea570463
jeffrey82221 <jeffrey82221@gmail.com>
2025-05-30 22:20:41 +0800

    feature: study the extending of yahoo input data

---
+++ package_study/backtest/extend_input.py
@@ -0,0 +1,82 @@
+"""
+Dollar Cost Averaging (DCA) Backtest Script
+"""
+import pandas as pd
+import backtesting
+from backtesting import Backtest
+from backtesting.test import GOOG
+import yfinance as yf
+
+def import_default_stock_data():
+    """
+    Import default stock data for backtesting.
+    """
+    return GOOG
+
+def import_custom_stock_data(symbol: str, start: str, end: str) -> pd.DataFrame:
+    """
+    Import custom stock data from Yahoo Finance.
+    
+    :param symbol: Stock symbol to import.
+    :param start: Start date for the data.
+    :param end: End date for the data.
+    :return: DataFrame containing stock data.
+    """
+    data = yf.Ticker(symbol)
+    ts = data.history(start=start, end=end)
+    return pd.DataFrame(ts)[['Open', 'High', 'Low', 'Close', 'Volume']]
+
+def assert_data_type_consistency(default_stock_data: pd.DataFrame, custom_stock_data: pd.DataFrame):
+    """
+    Assert that the DataFrame contains consistent data types.
+    
+    :param df: DataFrame to check.
+    """
+    assert isinstance(default_stock_data, pd.DataFrame), "Default stock data is not a DataFrame"
+    assert isinstance(custom_stock_data, pd.DataFrame), "Custom stock data is not a DataFrame"
+    assert default_stock_data.shape[0] > 0, "Default stock data is empty"
+    assert custom_stock_data.shape[0] > 0, "Custom stock data is empty"
+    assert default_stock_data.shape[1] == custom_stock_data.shape[1], "DataFrames have different number of columns"
+    assert set(default_stock_data.columns) == set(custom_stock_data.columns), "DataFrames have different columns"
+    assert default_stock_data.dtypes.equals(custom_stock_data.dtypes), "DataFrames have different data types"
+
+google_data = import_default_stock_data()
+# get start date of the google data
+start_date = google_data.index.min().strftime('%Y-%m-%d')
+# get end date + one day of the google data
+end_date = (google_data.index.max() + pd.Timedelta(days=1)).strftime('%Y-%m-%d')
+
+downloaded_google_data = import_custom_stock_data('GOOG', start_date, end_date)
+
+
+print('Default stock data:', google_data)
+print('Custom stock data:', downloaded_google_data)
+
+assert_data_type_consistency(
+    google_data, downloaded_google_data
+)
+
+# NOTE: Check by normalizing the data
+
+# Normalized the google_data to the same scale as the downloaded data
+# Only normalized the price related columns use for loops to avoid hardcoding column names
+
+google_open = google_data['Open'].iloc[0]
+downloaded_google_open = downloaded_google_data['Open'].iloc[0]
+
+for col in ['Open', 'High', 'Low', 'Close']:
+    google_data[col] = google_data[col] / google_open
+    downloaded_google_data[col] = downloaded_google_data[col] / downloaded_google_open
+    
+print('Default stock data:', google_data)
+print('Custom stock data:', downloaded_google_data)
+
+# Plot the open price of the default against that of the custom stock data
+google_data['Open'].plot(label='Default Google Open Price')
+downloaded_google_data['Open'].plot(label='Custom Google Open Price')
+import matplotlib.pyplot as plt
+plt.legend()
+plt.title('Google Open Price Comparison')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
--- package_study/vectorbt/load_data.py
+++ package_study/vectorbt/load_data.py
@@ -5,7 +5,7 @@ import datetime as dt
 
 symbol = '0050.TW'
 
-data = yf.Ticker(symbol) 
+data = yf.Ticker(symbol)
 ts = data.history(start='2021-01-01', end='2025-05-23')
 df = pd.DataFrame(ts) 
 print(df.columns)
