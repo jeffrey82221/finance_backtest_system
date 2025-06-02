e3d01dc71e64a0b8d6c6a9ac01c5f37f50695f0a
jeffrey82221 <jeffrey82221@gmail.com>
2025-06-03 07:18:11 +0800

    refactor: start learning script / add backtesting to indicator

--- package_study/backtest/add_indicator.py
+++ package_study/backtest/add_indicator.py
@@ -9,32 +9,52 @@ from ta import (
     add_volume_ta,
     add_others_ta
 )
+from backtesting import Backtest, Strategy
 from extend_input import import_custom_stock_data
 import vectorbt as vbt
 
+class RunInputAppendIndicator(Strategy):
+    """
+    Strategy to demonstrate how to append indicators to the backtesting framework.
+    This strategy is used to demonstrate how to add indicators to the backtesting framework.
+    It does not perform any trading actions.
+    """
+    def init(self):
+        pass
 
-# Prepare data
-start_date = '2025-01-01'  # crypto is in UTC
-end_date = '2025-05-23'
-downloaded_google_data = import_custom_stock_data('GOOG', start_date, end_date)
+    def next(self):
+        print('Trend MACD:', self.data.trend_macd[-1])
+        
+if __name__ == '__main__':
+    start_date = '2025-01-01'  # crypto is in UTC
+    end_date = '2025-05-23'
+    downloaded_google_data = import_custom_stock_data(
+        'GOOG', start_date, end_date)
+    google_with_all_indicators = add_all_ta_features(
+        downloaded_google_data.copy(),
+        open='Open',
+        high='High',
+        low='Low',
+        close='Close',
+        volume='Volume',
+        fillna=True
+    )
+    print('all_indicators:', google_with_all_indicators)
+    """
+    trend_indicators = add_trend_ta(
+        downloaded_google_data.copy(),
+        high='High',
+        low='Low',
+        close='Close',
+        fillna=True
+    ).columns
+    print('trend_indicators:', trend_indicators)
+    """
 
-all_indicators = add_all_ta_features(
-    downloaded_google_data.copy(),
-    open='Open',
-    high='High',
-    low='Low',
-    close='Close',
-    volume='Volume',
-    fillna=True
-).columns
-print('all_indicators:', all_indicators)
-
-trend_indicators = add_trend_ta(
-    downloaded_google_data.copy(),
-    high='High',
-    low='Low',
-    close='Close',
-    fillna=True
-).columns
-print('trend_indicators:', trend_indicators)
-
+    bt = Backtest(
+        google_with_all_indicators, 
+        RunInputAppendIndicator, 
+        trade_on_close=True
+    )
+    bt.run()
+    bt.plot()
